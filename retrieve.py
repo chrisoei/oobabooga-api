1f000931f749c2666c2ffd5f6e50fcb032b9ff92
Chris Oei <chris.oei@gmail.com>
2023-07-28 11:28:20 -0700

    Add script to retrieve result

---
+++ retrieve.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import json
+import psycopg2
+import sys
+
+def query(i):
+    sql1 = """
+        select model->'result'->'model_name', created_at, updated_at,
+               prompt, result
+        from oobabooga.api
+        where id = %s;
+    """
+    with psycopg2.connect() as db1:
+        with db1.cursor() as cur1:
+            cur1.execute(sql1, [ i ]) 
+            for row1 in cur1:
+                print("Model: {}".format(row1[0]))
+                print("Created at: {}". format(row1[1]))
+                print("Updated at: {}". format(row1[2]))
+                print("Prompt: {}". format(row1[3]))
+                print("Result: {}". format(row1[4]))
+
+if __name__ == '__main__':
+    query(sys.argv[1])
